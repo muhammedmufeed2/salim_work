838c7cccffb09a4d0d4fd1fdae77a8f752f6c8c3
muhammedmufeed2 <mohammedmufeed738@gmail.com>
2024-03-23 21:24:35 +0530

    s1

--- lib/Currencies.dart
+++ lib/Currencies.dart
@@ -28,139 +28,135 @@ class _CurrencesState extends State<Currences> {
           children: [
             Column(
               children: [
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 14, top: 5),
-                        child: Icon(
-                          Icons.menu,
-                          color: Colors.white30,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 65),
-                        child: Text(
-                          'Currency Converter',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 26,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w700,
-                            height: 0,
+                Padding(
+                  padding: const EdgeInsets.only(left: 14, top: 5),
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.white30,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 65),
+                  child: Text(
+                    'Currency Converter',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Check live rates, set rate alerts, receive notifications and more.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color(0xFF7F7F7F),
+                      fontSize: 16,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18, top: 5),
+                  child: Container(
+                    width: 320,
+                    height: 166,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 10, top: 5),
+                          child: Text(
+                            'Converted Amount',
+                            style: TextStyle(
+                              color: Color(0xFF979797),
+                              fontSize: 15,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w400,
+                              height: 0,
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Text(
-                          'Check live rates, set rate alerts, receive notifications and more.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color(0xFF7F7F7F),
-                            fontSize: 16,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w400,
-                            height: 0,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 18, top: 5),
-                        child: Container(
-                          width: 320,
-                          height: 166,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 10, top: 5),
-                                child: Text(
-                                  'Converted Amount',
-                                  style: TextStyle(
-                                    color: Color(0xFF979797),
-                                    fontSize: 15,
-                                    fontFamily: 'Roboto',
-                                    fontWeight: FontWeight.w400,
-                                    height: 0,
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 15, top: 8),
+                              child: ClipOval(
+                                child: Image.asset(
+                                  "assets/Flag-United-States-of-America.webp",
+                                  width: 45,
+                                  height: 45,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 10, top: 5),
+                              child: Text(
+                                'USD',
+                                style: TextStyle(
+                                  color: Color(0xFF25278D),
+                                  fontSize: 20,
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.w500,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5),
+                              child: Icon(
+                                Icons.arrow_drop_down,
+                                color: Colors.black45,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15),
+                              child: Container(
+                                width: 141,
+                                height: 45,
+                                decoration: ShapeDecoration(
+                                  color: Color(0xFFEEEEEE),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(7)),
+                                ),
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(right: 10),
+                                  child: Text(
+                                    '736.70',
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(
+                                      color: Color(0xFF3C3C3C),
+                                      fontSize: 22,
+                                      fontFamily: 'Roboto',
+                                      fontWeight: FontWeight.w600,
+                                      height: 0,
+                                    ),
                                   ),
                                 ),
                               ),
-                              Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 15, top: 8),
-                                    child: ClipOval(
-                                      child: Image.asset(
-                                        "assets/Flag-United-States-of-America.webp",
-                                        width: 45,
-                                        height: 45,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 10, top: 5),
-                                    child: Text(
-                                      'USD',
-                                      style: TextStyle(
-                                        color: Color(0xFF25278D),
-                                        fontSize: 20,
-                                        fontFamily: 'Roboto',
-                                        fontWeight: FontWeight.w500,
-                                        height: 0,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 5),
-                                    child: Icon(
-                                      Icons.arrow_drop_down,
-                                      color: Colors.black45,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 15),
-                                    child: Container(
-                                      width: 141,
-                                      height: 45,
-                                      decoration: ShapeDecoration(
-                                        color: Color(0xFFEEEEEE),
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(7)),
-                                      ),
-                                      child: Padding(
-                                        padding:
-                                            const EdgeInsets.only(right: 10),
-                                        child: Text(
-                                          '736.70',
-                                          textAlign: TextAlign.right,
-                                          style: TextStyle(
-                                            color: Color(0xFF3C3C3C),
-                                            fontSize: 22,
-                                            fontFamily: 'Roboto',
-                                            fontWeight: FontWeight.w600,
-                                            height: 0,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ]),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
 
                 Padding(
                   padding: const EdgeInsets.only(top: 20,bottom:20),
